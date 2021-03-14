6e013f953d029846139714fda3d944444b15e231
IngridCBarbosa <ingridcarolinelima@hotmail.com>
2021-03-14 19:45:25 -0300

    Adding react without create-react-app just webpack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 const name = 'Ingrid';
+
+ReactDOM.render(<div>Nome: {name}</div>, document.querySelector('#app'))
 
 console.log('Funcionou');
